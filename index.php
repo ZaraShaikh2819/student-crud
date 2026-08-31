18e757d272f0dbcea4af4911d494a90c2b056e42
ZaraShaikh2819 <zarashaikh2819@gmail.com>
2026-08-31 19:58:25 +0530

    index page edited

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <nav class="navbar bg-body-tertiary">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="uploads/logo.svg" alt="Bootstrap" width="30" height="24">
+                <img src="uploads/logo.svg" alt="Bootstrap" height="50">
             </a>
             <ul class="nav justify-content-end">
                 <li>
@@ -57,26 +57,66 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
-        <figure class="text-end">
-            <blockquote class="blockquote">
-                <p>Harvard University is a private Ivy League research university in Cambridge, Massachusetts,
-                    United States. Founded in 1636, and named Harvard College in 1639 in honor of its first
-                    benefactor, Puritan clergyman John Harvard, it is the oldest institution of higher learning in
-                    the United States. Its influence, wealth, and rankings have made it one of the most prestigious
-                    universities in the world.
+        <br><br>
+        <div class="card mb-3" style="max-width: 1250px;">
+            <div class="row g-0">
+                <div class="col-md-8">
+                    <img src="uploads/2.jpg" class="img-fluid rounded-start" alt="..." height="200">
+                </div>
+                <div class="col-md-4">
+                    <div class="card-body">
+                        <h5 class="card-title">About US</h5>
+                        <p class="card-text">
+                        <p>Harvard University is a private Ivy League research university in Cambridge, Massachusetts,
+                            United States. Founded in 1636, and named Harvard College in 1639 in honor of its first
+                            benefactor, Puritan clergyman John Harvard, it is the oldest institution of higher learning
+                            in
+                            the United States. Its influence, wealth, and rankings have made it one of the most
+                            prestigious
+                            universities in the world.
 
-                    Harvard was founded and authorized by the Massachusetts General Court, the governing legislature
-                    of colonial-era Massachusetts Bay Colony.While never formally affiliated with any
-                    Protestant denomination, Harvard trained Congregational clergy until its curriculum and student
-                    body were gradually secularized in the 18th century. In 1780, the Constitution of Massachusetts
-                    referred to it as a university, and a new medical school established in 1782 was named the
-                    Medical Institution of Harvard University; but this was a new name, not a new foundation.
-                    Harvard College soon became the name of the university's undergraduate division.</p>
-            </blockquote>
-            <figcaption class="blockquote-footer">
-                Someone famous in <cite title="Source Title">Source Title</cite>
-            </figcaption>
+                            Harvard was founded and authorized by the Massachusetts General Court, the governing
+                            legislature
+                            of colonial-era Massachusetts Bay Colony.While never formally affiliated with any
+                            Protestant denomination, Harvard trained Congregational clergy until its curriculum and
+                            student
+                            body were gradually secularized in the 18th century. In 1780, the Constitution of
+                            Massachusetts
+                            referred to it as a university, and a new medical school established in 1782 was named the
+                            Medical Institution of Harvard University; but this was a new name, not a new foundation.
+                            Harvard College soon became the name of the university's undergraduate division.
+                            Harvard's endowment, valued at $55.7 billion, makes it the wealthiest academic institution
+                            in the
+                            world.[20][21][22] Harvard Library, with more than 20 million volumes, is the world's
+                            largest academic library.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+            Someone famous in <cite title="Source Title">Source Title</cite>
+        </figcaption>
         </figure>
+    </div>
+    <div class="card mb-12" style="max-width: 1250px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="uploads/logo.svg" class="img-fluid rounded-start" alt="..." height="400">
+            </div>
+            <div class="col-md-4">
+                <div class="card-body">
+                    <p><a href="#" class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Secondary link</a></p>
+                    <p><a href="#" class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Secondary link</a></p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
