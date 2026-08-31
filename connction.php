e695406684253ee30f654fbc0960d8e9207057cf
ZaraShaikh2819 <zarashaikh2819@gmail.com>
2026-08-31 21:02:30 +0530

    connection page added

--- connction.php
+++ connction.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "root123", "scholl_db");
+$conn = mysqli_connect("localhost", "root", "root123", "school_db");
 if (!$conn) {
     die("Connection Failed.");
 }
