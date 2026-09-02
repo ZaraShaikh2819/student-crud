cc21611a018e0d7c38e4a538dfb4f7ba037c6172
ZaraShaikh2819 <zarashaikh2819@gmail.com>
2026-09-02 16:22:40 +0530

    edited page index.php

--- index.php
+++ index.php
@@ -4,102 +4,146 @@
 <head>
     <title>Index Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="uploads/logo.svg" alt="Bootstrap" height="50">
-            </a>
-            <ul class="nav justify-content-end">
-                <li>
-                    <a class="nav-link" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="about.php">Abut Us</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <div id="carouselExampleCaptions" class="carousel slide">
-        <div class="carousel-indicators p-10">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="uploads/1.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Harverd University</h5>
-                    <p>Best Univerty Around the world.</p>
-                </div>
+    <div class="header">
+        <nav class="navbar nav">
+            <div class="container navbar">
+                <a class="navbar-brand" href="#">
+                    <img src="uploads/logo.svg" alt="Bootstrap" height="50">
+                </a>
+                <ul class="nav justify-content-end">
+                    <li>
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">Abut Us</a>
+                    </li>
+                </ul>
             </div>
-            <div class="carousel-item">
-                <img src="uploads/2.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Harverd University</h5>
-                    <p>Best Univerty Around the world.</p>
-                </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-        <br><br>
-        <br><br>
-        <div class="card mb-3" style="max-width: 1250px;">
-            <div class="row g-0">
-                <div class="col-md-8 p-30">
-                    <img src="uploads/2.jpg" class="img-fluid rounded-start" alt="..." height="200">
-                </div>
-                <div class="col-md-4">
-                    <div class="card-body">
-                        <h5 class="card-title">About US</h5>
-                        <p class="card-text">
-                        <p>Harvard University is a private Ivy League research university in Cambridge, Massachusetts,
-                            United States. Founded in 1636, and named Harvard College in 1639 in honor of its first
-                            benefactor, Puritan clergyman John Harvard, it is the oldest institution of higher learning
-                            in
-                            the United States. Its influence, wealth, and rankings have made it one of the most
-                            prestigious
-                            universities in the world.
+        </nav>
+        <p class="divpara">
+        <h1 class="p">Welcome To Harvard's</h1>
+        <i>Talented students and world-class faculty come together at Harvard to <br>
+            collaborate on groundbreaking research,cutting-edge innovations,<br>
+            and transformative scholarly work.</i>
+        </p>
+    </div>
 
-                            Harvard was founded and authorized by the Massachusetts General Court, the governing
-                            legislature
-                            of colonial-era Massachusetts Bay Colony.While never formally affiliated with any
-                            Protestant denomination, Harvard trained Congregational clergy until its curriculum and
-                            student
-                            body were gradually secularized in the 18th century. In 1780, the Constitution of
-                            Massachusetts
-                            referred to it as a university, and a new medical school established in 1782 was named the
-                            Medical Institution of Harvard University; but this was a new name, not a new foundation.
-                            Harvard College soon became the name of the university's undergraduate division.
-                            Harvard's endowment, valued at $55.7 billion, makes it the wealthiest academic institution
-                            in the
-                            world.[20][21][22] Harvard Library, with more than 20 million volumes, is the world's
-                            largest academic library.
-                        </p>
+    <br><br>
+    <table width="1250px" height="400px">
+        <tr>
+            <th>
+                <h1 class="p float-start">Explore Our Campus</h1><br><br><br>
+                <p class="p">Explore our beautiful campus, discover inspiring spaces,<br>
+                    and experience a vibrant environment<br>
+                    designed for learning, creativity, and growth.
+                </p>
+            </th>
+            <th width="600px">
+                <div class="card mb-3 float-end">
+                    <div class="row g-0">
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="uploads/2.jpg" class="d-block w-100" alt="..." width="150px"
+                                        height="300px">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="uploads/3.jpg" class="d-block w-100" alt="..." width="150px"
+                                        height="300px">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="uploads/4.jpg" class="d-block w-100" alt="..." width="150px"
+                                        height="300px">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </th>
+        </tr>
+    </table>
+    <div class="card mb-12 row g-0" style="max-width: 1250px;max-height: 700px">
+        <br>
+        <div class="col-md-4 float-start">
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="uploads/5.jpg" class="d-block w-100" alt="..." width="200px" height="400px">
+                                <h2 class="p">Our Museum</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="uploads/6.jpg" class="d-block w-100" alt="..." width="200px" height="400px">
+                                <h2 class="p">Our Museum</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="uploads/7.jpg" class="d-block w-100" alt="..." width="200px" height="400px">
+                                <h2 class="p">Our Museum</h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        </blockquote>
-        <figcaption class="blockquote-footer">
-            Someone famous in <cite title="Source Title">Source Title</cite>
-        </figcaption>
-        </figure>
+        <div class="col-md-4 float-md-none">
+            <div class="card-body">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="uploads/8.jpg" class="d-block w-100" alt="..." width="200px"
+                                        height="400px">
+                                    <h2 class="p">Our Library</h2>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="uploads/9.jpg" class="d-block w-100" alt="..." width="200px"
+                                        height="400px">
+                                    <h2 class="p">Our Library</h2>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="uploads/10.jpg" class="d-block w-100" alt="..." width="200px"
+                                        height="400px">
+                                    <h2 class="p">Our Library</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 float-end">
+            <div class="card-body">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="uploads/11.jpg" class="d-block w-100" alt="..." width="200px"
+                                        height="400px">
+                                    <h2 class="p">Our Athelics</h2>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="uploads/12.jpg" class="d-block w-100" alt="..." width="200px"
+                                        height="400px">
+                                    <h2 class="p">Our Athelics</h2>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="uploads/13.jpg" class="d-block w-100" alt="..." width="200px"
+                                        height="400px">
+                                    <h2 class="p">Our Athelics</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="card mb-12" style="max-width: 1250px;">
         <div class="row g-0">
